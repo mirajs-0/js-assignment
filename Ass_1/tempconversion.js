f8c857fc3bd7f0efd3a86710248f5b647c823e6b
Miraj Shrestha <mirajs@metropolia.fi>
2025-10-25 13:24:08 +0545

    Assignment number 1 completed

---
+++ Ass_1/tempconversion.js
@@ -0,0 +1,26 @@
+var convertBtn = document.getElementById("convertBtn");
+var celsiusInput = document.getElementById("celsius");
+
+// Run the conversion
+convertBtn.onclick = function () {
+  var celsius = celsiusInput.value;
+
+  // Check if the input is empty or not a number
+  if (celsius === "" || isNaN(celsius)) {
+    alert("Please enter a valid number for Celsius.");
+  } else {
+    // Convert Celsius to Fahrenheit
+    var fahrenheit = (celsius * 9) / 5 + 32;
+
+    // Convert Celsius to Kelvin
+    var kelvin = parseFloat(celsius) + 273.15;
+
+    // Display the results in the HTML
+    document.getElementById("celsiusResult").innerText =
+      "Temperature in Celsius: " + celsius + "°C";
+    document.getElementById("fahrenheitResult").innerText =
+      "Temperature in Fahrenheit: " + fahrenheit + "°F";
+    document.getElementById("kelvinResult").innerText =
+      "Temperature in Kelvin: " + kelvin + " K";
+  }
+};
